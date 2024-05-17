0574479279b9bfa07ba56cde363ed46ca59fa6ed
rafael-men <rafaelgoncalves1822@gmail.com>
2024-05-17 11:43:33 -0300

    done

--- Calculator/app/build.gradle.kts
+++ Calculator/app/build.gradle.kts
@@ -1,13 +1,11 @@
-
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-android-extensions")
 }
 
 android {
-    namespace = "com.example.calculator"
-    compileSdk = 30
+    compileSdkVersion(30)
 
     defaultConfig {
         applicationId = "com.example.calculator"
@@ -15,33 +13,37 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildToolsVersion = "30.0.3"
+
+
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("org.mariuszgromada.math:MathParser.org-mXparser:4.4.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.calculator
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import missing.namespace.R
+import org.mariuszgromada.math.mxparser.Expression
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,12 +15,98 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        button_clear.setOnClickListener {
+            input.text = ""
+            output.text = ""
         }
+
+        bracket_left.setOnClickListener {
+            input.text = addToInputText("(")
+        }
+        bracket_right.setOnClickListener {
+            input.text = addToInputText(")")
+        }
+
+        button_0.setOnClickListener {
+            input.text = addToInputText("0")
+        }
+        button_1.setOnClickListener {
+            input.text = addToInputText("1")
+        }
+        button_2.setOnClickListener {
+            input.text = addToInputText("2")
+        }
+        button_3.setOnClickListener {
+            input.text = addToInputText("3")
+        }
+        button_4.setOnClickListener {
+            input.text = addToInputText("4")
+        }
+        button_5.setOnClickListener {
+            input.text = addToInputText("5")
+        }
+        button_6.setOnClickListener {
+            input.text = addToInputText("6")
+        }
+        button_7.setOnClickListener {
+            input.text = addToInputText("7")
+        }
+        button_8.setOnClickListener {
+            input.text = addToInputText("8")
+        }
+        button_9.setOnClickListener {
+            input.text = addToInputText("9")
+        }
+        button_dot.setOnClickListener {
+            input.text = addToInputText(".")
+        }
+        button_division.setOnClickListener {
+            input.text = addToInputText("÷") // ALT + 0247
+        }
+        button_multiplication.setOnClickListener {
+            input.text = addToInputText("×") // ALT + 0215
+        }
+        button_subtraction.setOnClickListener {
+            input.text = addToInputText("-")
+        }
+        button_addition.setOnClickListener {
+            input.text = addToInputText("+")
+        }
+
+        button_equals.setOnClickListener {
+            //return
+        }
+
+
     }
 
+    private fun addToInputText(buttonValue: String): String {
+        return "${input.text}$buttonValue"
+    }
+    private fun getInputExpression(): String {
+        var expression = input.text.replace(Regex("/"), "/")
+        expression = expression.replace(Regex("x"), "*")
+        return expression
+    }
+
+    private fun showResult() {
+        try {
+            val expression = getInputExpression()
+            val result = Expression(expression).calculate()
+            if (result.isNaN()) {
+
+                output.text = "Error"
+                output.setTextColor(ContextCompat.getColor(this, R.color.red))
+            } else {
+
+                output.text = DecimalFormat("0.######").format(result).toString()
+                output.setTextColor(ContextCompat.getColor(this, R.color.green))
+            }
+        } catch (e: Exception) {
+
+            output.text = "Error"
+            output.setTextColor(ContextCompat.getColor(this, R.color.red))
+        }
+    }
 
 }
